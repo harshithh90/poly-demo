c8db14eeafbc1911044225e05028ae6550ca17f8
harshithh90 <149898021+harshithh90@users.noreply.github.com>
2026-02-02 16:58:25 +0530

    Adjust wait time and refine element interaction methods
    Increased wait time for element clickability and changed visibility check to presence check.

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,21 +1,15 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import ElementClickInterceptedException
 
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
-        self.wait = WebDriverWait(driver, 10)
+        self.wait = WebDriverWait(driver, 30)
 
     def click(self, locator):
-        element = self.wait.until(EC.element_to_be_clickable(locator))
-        try:
-            element.click()
-        except ElementClickInterceptedException:
-            # Fallback to JavaScript click
-            self.driver.execute_script("arguments[0].click();", element)
+        self.wait.until(EC.element_to_be_clickable(locator)).click()
 
     def type(self, locator, text):
-        el = self.wait.until(EC.visibility_of_element_located(locator))
+        el = self.wait.until(EC.presence_of_element_located(locator))
         el.clear()
         el.send_keys(text)
