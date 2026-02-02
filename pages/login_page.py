31887ad9f9624c4127fb479d9fc32615f947c4f3
harshithh90 <149898021+harshithh90@users.noreply.github.com>
2026-02-02 16:57:57 +0530

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,13 +1,31 @@
 from selenium.webdriver.common.by import By
-from pages.base_page import BasePage
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
-class LoginPage(BasePage):
+import config
 
+
+class LoginPage:
     EMAIL = (By.XPATH, "//input[@id='login-email']")
     PASSWORD = (By.XPATH, "//input[@id='login-input-password']")
     LOGIN_BTN = (By.XPATH, "//button[text()='LOG IN']")
 
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 30)
+
+    def open(self):
+        self.driver.get(config.BASE_URL)
+
     def login(self, username, password):
-        self.type(self.EMAIL, username)
-        self.type(self.PASSWORD, password)
-        self.click(self.LOGIN_BTN)
+        self.open()  # 🔥 CRITICAL FIX
+
+        email_el = self.wait.until(EC.presence_of_element_located(self.EMAIL))
+        email_el.clear()
+        email_el.send_keys(username)
+
+        password_el = self.wait.until(EC.presence_of_element_located(self.PASSWORD))
+        password_el.clear()
+        password_el.send_keys(password)
+
+        self.wait.until(EC.element_to_be_clickable(self.LOGIN_BTN)).click()
