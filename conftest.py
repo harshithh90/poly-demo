cf327a4090701251b887c488ffd498aaa5ba16c8
harshithh90 <149898021+harshithh90@users.noreply.github.com>
2026-02-02 15:21:12 +0530

    Modify Chrome driver setup in conftest.py
    Updated Chrome driver options for headless mode and removed unused imports.

--- conftest.py
+++ conftest.py
@@ -1,18 +1,18 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-import config
-
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture
 def driver():
-    options = webdriver.ChromeOptions()
-    options.add_argument("--start-maximized")
-    options.add_argument("--disable-extensions")
-    options.add_argument("--disable-plugins")
-    options.add_argument("--disable-images")
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-    driver.get(config.BASE_URL)
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(options=options)
+    driver.maximize_window()
+
     yield driver
+
     driver.quit()
+
